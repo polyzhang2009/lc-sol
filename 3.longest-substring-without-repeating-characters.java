d0b0ddcceeb50dd86dca1559e75ea1dbc0419136
hustbill <billyzhang2010@gmail.com>
2024-06-20 11:27:09 -0700

    merge main to dev

---
+++ 3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            
+        }
+    }
+}
+// @lc code=end
+
---
+++ 794.valid-tic-tac-toe-state.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=794 lang=java
+ *
+ * [794] Valid Tic-Tac-Toe State
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validTicTacToe(String[] board) {
+        
+    }
+}
+// @lc code=end
+
