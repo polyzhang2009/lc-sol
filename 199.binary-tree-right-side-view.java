64ee71976ddfb8fd285415abe577c9ce5b5d5564
Poly Zhang <polyzhang2009@gmail.com>
2024-06-24 10:53:48 -0700

    199.binary-tree-right-side-view.java

---
+++ 199.binary-tree-right-side-view.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=199 lang=java
+ *
+ * [199] Binary Tree Right Side View
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+
+        return res;
+    }
+}
+// @lc code=end
+
